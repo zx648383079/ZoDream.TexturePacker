5dab74c8ad672254d0e7aa771ae4d906dd9fa084
ZoDream <648383079@qq.com>
2026-01-28 16:23:10 +0800

    update

---
+++ src/ZoDream.Shared.ImageEditor/Source/RulerImageSource.cs
@@ -0,0 +1,76 @@
+﻿using SkiaSharp;
+using System.Collections.Generic;
+using ZoDream.Shared.EditorInterface;
+
+namespace ZoDream.Shared.ImageEditor
+{
+    public class RulerImageSource(IImageEditor editor) : BaseImageSource(editor), ICommandImageSource
+    {
+
+
+        private readonly HashSet<int> _horizontalLines = [];
+        private readonly HashSet<int> _verticalLines = [];
+        private SKSurface? _surface;
+
+        public void With(IImageLayer layer)
+        {
+        }
+
+        public void Resize(float width, float height)
+        {
+            Invalidate();
+        }
+
+        public void Invalidate()
+        {
+            Width = Editor.ActualWidthI;
+            Height = Editor.ActualHeightI;
+            _surface?.Dispose();
+            _surface = null;
+        }
+
+        public void Paint(IImageCanvas canvas)
+        {
+            if (_surface == null)
+            {
+                RenderSurface();
+            }
+            if (_surface == null)
+            {
+                return;
+            }
+            canvas.DrawSurface(_surface, 0, 0);
+        }
+
+        private void RenderSurface()
+        {
+            if (Width == 0 || Height == 0)
+            {
+                return;
+            }
+            var info = new SKImageInfo((int)Width, (int)Height);
+            _surface = SKSurface.Create(info);
+            var canvas = _surface.Canvas;
+            canvas.Clear(SKColors.Transparent);
+            using var paint = new SKPaint()
+            {
+                Color = SKColors.Green,
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 1,
+            };
+            foreach (var item in _horizontalLines)
+            {
+                canvas.DrawLine(0, item, Width, item, paint);
+            }
+            foreach (var item in _verticalLines)
+            {
+                canvas.DrawLine(item, 0, item, Height, paint);
+            }
+        }
+
+        public override void Dispose()
+        {
+            _surface?.Dispose();
+        }
+    }
+}
---
+++ src/plugins/MonoGame/TextureAtlasReader.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using ZoDream.Shared.Interfaces;
+using ZoDream.Shared.IO;
+using ZoDream.Shared.Models;
+
+namespace ZoDream.Plugin.MonoGame
+{
+    public class TextureAtlasReader : BaseTextReader
+    {
+        public override bool IsEnabled(string content)
+        {
+            return content.Contains("</TextureAtlas>");
+        }
+
+        public override IEnumerable<ISpriteSection>? Deserialize(string content, string fileName)
+        {
+            var res = new SpriteLayerSection();
+            using var reader = XmlReader.Create(new StringReader(content));
+            var doc = XDocument.Load(reader);
+            var root = doc.Root;
+            res.Name = res.FileName = root.Element("Texture").Value;
+            var regions = root.Element("Regions")?.Elements("Region");
+
+            if (regions != null)
+            {
+                foreach (var region in regions)
+                {
+                    var name = region.Attribute("name")?.Value;
+                    int x = int.Parse(region.Attribute("x")?.Value ?? "0");
+                    int y = int.Parse(region.Attribute("y")?.Value ?? "0");
+                    int width = int.Parse(region.Attribute("width")?.Value ?? "0");
+                    int height = int.Parse(region.Attribute("height")?.Value ?? "0");
+
+                    res.Items.Add(new SpriteLayer()
+                    {
+                        Name = name ?? string.Empty,
+                        X = x,
+                        Y = y,
+                        Width = width,
+                        Height = height
+                    });
+                }
+            }
+
+            return [res];
+        }
+
+        public override string Serialize(IEnumerable<ISpriteSection> data, string fileName)
+        {
+            var sb = new StringBuilder();
+            using var writer = XmlWriter.Create(sb);
+            foreach (var item in data)
+            {
+                writer.WriteStartDocument(false);
+                writer.WriteStartElement("TextureAtlas");
+                writer.WriteStartElement("Texture");
+                writer.WriteString(item.Name ?? Path.GetFileNameWithoutExtension(fileName));
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("Regions");
+
+                foreach (var layer in item.Items)
+                {
+                    writer.WriteStartElement("Region");
+                    writer.WriteAttributeString("name", layer.Name);
+                    writer.WriteAttributeString("x", layer.X.ToString());
+                    writer.WriteAttributeString("y", layer.Y.ToString());
+                    writer.WriteAttributeString("width", layer.Width.ToString());
+                    writer.WriteAttributeString("height", layer.Height.ToString());
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+            return sb.ToString();
+        }
+    }
+}
